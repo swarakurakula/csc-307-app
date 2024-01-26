b34fb8d071ffe22aa457ba30799b1f6cc66a9055
swarakurakula <102617350+swarakurakula@users.noreply.github.com>
2024-01-25 17:27:49 -0800

    finished step 5: getting users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,10 +35,11 @@ const users = {
 };
 
 const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-  };
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.use(express.json());
 
@@ -47,15 +48,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
-  });
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
